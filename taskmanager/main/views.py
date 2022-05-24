6a6d8dd4906e11141aedf2743dc4ed487b090c94
Andrew <andrew.am2017@gmail.com>
2022-05-24 22:55:09 +0300

    вывел список задач на главную

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . models import Task
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'main/index.html', {})
+    all_tasks = Task.objects.all()
+    title_page = 'Главная страница'
+    return render(request, 'main/index.html', {'all_tasks': all_tasks, 'title_page': title_page})
 
 
 def about(request):
-    return render(request, 'main/about.html', {})
+    title_page = 'Информация о нас'
+    return render(request, 'main/about.html', {'title_page': title_page})
