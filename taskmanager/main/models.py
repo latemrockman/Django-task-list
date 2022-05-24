d951817578706b2955ebc88392297319c845ae95
Andrew <andrew.am2017@gmail.com>
2022-05-24 22:35:04 +0300

    подключил таблицу в админке и создал первые задачи из админки

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -7,6 +7,12 @@ class Task(models.Model):
     title = models.CharField('Название', max_length=50)
     task = models.TextField('Описание')
 
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+
+
 
     def __str__(self):
         return self.title
